9ac0899127d30a73dc0cd77d88730ebe164b0a80
Stuart Willows <SJWillows91@outlook.com>
2022-12-21 22:28:13 +0000

    Initial Project Creation

---
+++ ShiftTracker/ShiftTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ShiftTracker.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty( RequestId );
+}
