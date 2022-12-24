cdbe9a59f8368da6da7c3291a1983cbbc5ea4129
Stuart Willows <SJWillows91@outlook.com>
2022-12-24 01:29:23 +0000

    Added Shift and Break Models

---
+++ ShiftTracker/ShiftTracker/Areas/Shifts/Models/Break.cs
@@ -0,0 +1,14 @@
+namespace ShiftTracker.Areas.Shifts.Models;
+
+public class Break
+{
+    public int Id { get; set; }
+    public DateTime Date { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public TimeSpan Duration { get; set; }
+
+    // Navigation properties
+    public int ShiftId { get; set; }
+    public Shift Shift { get; set; }
+}
