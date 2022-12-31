39a6170b5b9b6e13df622a4b4387e410f3b8fb15
Stuart Willows <SJWillows91@outlook.com>
2022-12-31 16:39:12 +0000

    Added Typescript & Gulp

---
+++ ShiftTracker/ShiftTracker/wwwroot/scripts/app.js
@@ -0,0 +1,9 @@
+function sayHello() {
+    const compiler = document.getElementById("compiler")
+        .value;
+    const framework = document.getElementById("framework")
+        .value;
+    return `Hello from ${compiler} and ${framework}!`;
+}
+console.log("Hello World");
+//# sourceMappingURL=app.js.map
