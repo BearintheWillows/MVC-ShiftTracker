484177d80d75b1a01cdd40f2fa5b65c6715e2eec
Stuart Willows <SJWillows91@outlook.com>
2022-12-31 19:50:05 +0000

    Added Generic Base Api Controller and Concrete Shifts Controller

---
+++ ShiftTracker/ShiftTracker/Controllers/BaseApiController.cs
@@ -0,0 +1,51 @@
+﻿namespace ShiftTracker.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+using Data;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
+public abstract class BaseApiController<TEntity> : ControllerBase where TEntity : class
+{
+	private readonly ApplicationDbContext _context;
+
+
+	protected BaseApiController(ApplicationDbContext context)
+	{
+		_context = context;
+	}
+
+	public async Task<IActionResult> GetAll()
+	{
+		try
+		{
+			List<TEntity> result = await _context.Set<TEntity>().ToListAsync();
+			Log.Information( $"List of {typeof(TEntity).Name} returned successfully" );
+			Log.Information( $"{typeof(TEntity).Name} contains {result.Count} record." );
+			return Ok( result );
+		}
+		catch ( Exception e )
+		{
+			Console.WriteLine( e );
+			Log.Error( e, "Get all {0}", typeof(TEntity).Name );
+			return BadRequest( e.Message );
+		}
+	}
+
+	public async Task<IActionResult> Get(int id)
+	{
+		try
+		{
+			var result = await _context.Set<TEntity>().FindAsync( id );
+			Log.Information( $"Get {typeof(T).Name} with id {id} SUCCESS" );
+			return Ok( result );
+		}
+		catch ( Exception e )
+		{
+			Log.Error( $"Get {typeof(T)} with id {id} FAILED" );
+			Log.Debug( e.Message );
+			return BadRequest( e.Message );
+		}
+	
+	}
+}
---
+++ ShiftTracker/ShiftTracker/Controllers/ShiftsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShiftTracker.Controllers;
+
+using Areas.Shifts.Models;
+using Data;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ShiftsController : BaseApiController<Shift>
+{
+
+	public ShiftsController(ApplicationDbContext context) : base( context )
+	{
+		
+	}
+	
+}
