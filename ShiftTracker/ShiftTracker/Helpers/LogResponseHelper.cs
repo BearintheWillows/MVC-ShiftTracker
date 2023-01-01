3873e9dd4691c114c91c1110586a3d83923001e2
Stuart Willows <SJWillows91@outlook.com>
2023-01-01 07:50:58 +0000

    Added LogHelperResponse class

--- ShiftTracker/ShiftTracker/Helpers/LogResponseHelper.cs
+++ ShiftTracker/ShiftTracker/Helpers/LogResponseHelper.cs
@@ -1,9 +1,23 @@
 ﻿namespace ShiftTracker.Helpers;
 
-public static class LogHelper
+public static class LogResponseHelper
 {
-	public static String RetrieveSuccess(Type T)
+	public static String GetDBSuccess(Type T)
 	{
-			return $"{T.Name} was successfully retrieved.";
+		return $"Successfully retrieved {T.Name} from database.";
 	}
+	
+	public static string CollectionCount(Type T, Object obj)
+	{
+		return $"{T.Name} collection count: {obj}";
+	}
+
+	
+	public static String GetDBError(Type T)
+	{
+		return $"{T.Name} could not be found in database.";
+	}
+	
+	
+
 }
