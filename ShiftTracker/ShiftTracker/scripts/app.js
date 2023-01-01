d2c60da0be695f263892a64905893284dbaa2cdb
Stuart Willows <SJWillows91@outlook.com>
2023-01-01 07:51:53 +0000

    Added TS methods to retrieve all shifts and shift by id

--- ShiftTracker/ShiftTracker/scripts/app.js
+++ ShiftTracker/ShiftTracker/scripts/app.js
@@ -1,9 +1,29 @@
-function sayHello() {
-    const compiler = document.getElementById("compiler")
-        .value;
-    const framework = document.getElementById("framework")
-        .value;
-    return `Hello from ${compiler} and ${framework}!`;
+class ShiftDataAccess {
+    constructor() {
+    }
+    shifts = [];
+    shift = null;
+    initProperties() {
+        this.getAllShifts().then((shifts) => {
+            this.shifts = shifts;
+            console.log(this.shifts);
+        });
+        this.getShift(-1).then((shift) => {
+            this.shift = shift;
+            console.log(this.shift);
+        });
+    }
+    async getAllShifts() {
+        let response = await fetch("/api/Shifts");
+        let shifts = await response.json();
+        return shifts;
+    }
+    async getShift(id) {
+        let response = await fetch(`/api/Shifts/${id}`);
+        let shift = await response.json();
+        return shift;
+    }
 }
-console.log("Hello World");
+let shiftDataAccess = new ShiftDataAccess();
+shiftDataAccess.initProperties();
 //# sourceMappingURL=app.js.map
--- ShiftTracker/ShiftTracker/scripts/app.ts
+++ ShiftTracker/ShiftTracker/scripts/app.ts
@@ -1,9 +1,53 @@
-﻿function sayHello() {
-    const compiler = (document.getElementById("compiler") as HTMLInputElement)
-        .value;
-    const framework = (document.getElementById("framework") as HTMLInputElement)
-        .value;
-    return `Hello from ${compiler} and ${framework}!`;
+﻿interface IBreak {
+    Id: number;
+    StartTime: Date;
+    EndTime: Date;
+    ShiftId: number;
+    Shift: IShift;
+}
+interface IShift {
+    Id: number;
+    RunId: number;
+    StartTime: Date;
+    EndTime: Date;
+    TotalBreakLength: Date;
+    TotalShiftLength: Date;
+    TotalDriveLength: Date;
+    TotalWorkLength: Date;
+    Breaks: IBreak[];
 }
 
-console.log("Hello World");
+class ShiftDataAccess{
+    constructor() {
+    }
+    shifts: IShift[] = [];
+    shift: IShift = null;
+    
+    initProperties(): void {
+        this.getAllShifts().then((shifts) => {
+            this.shifts = shifts;
+            console.log(this.shifts);
+        });
+        this.getShift(-1).then((shift) => {
+            this.shift = shift;
+            console.log(this.shift);
+        });
+    }
+    async getAllShifts(): Promise<IShift[]> {
+        let response = await fetch("/api/Shifts");
+        let shifts = await response.json();
+        return shifts;
+    }
+    
+    async getShift(id: number): Promise<IShift> {
+        let response = await fetch(`/api/Shifts/${id}`);
+        let shift = await response.json();
+        return shift;
+    }
+}
+
+let shiftDataAccess = new ShiftDataAccess();
+
+shiftDataAccess.initProperties();
+
+
