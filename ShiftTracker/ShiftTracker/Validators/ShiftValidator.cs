abff1efe55a1b1a7d4b02caa67e5756045c6c345
Stuart Willows <SJWillows91@outlook.com>
2023-01-05 14:11:28 +0000

    Added Validator section for any Validation logic

--- ShiftTracker/ShiftTracker/Validators/ShiftValidator.cs
+++ ShiftTracker/ShiftTracker/Validators/ShiftValidator.cs
@@ -1,6 +1,17 @@
 ﻿namespace ShiftTracker.Pages.Validators;
 
-public class ShiftValidator
+using Data.Models;
+using ShiftTracker.Areas.Shifts.Models.DTO;
+
+public static class ShiftValidator
 {
-	
+	/// <summary>
+	/// Checks if the shift times add up to the shift duration.
+	/// </summary>
+	/// <param name="shiftDto"></param>
+	/// <returns>True/False</returns>
+	public static bool TimeEntryValidation(ShiftDto shiftDto)
+	{
+		return shiftDto.ShiftDuration.Equals( new TimeSpan(shiftDto.BreakDuration.Ticks + shiftDto.WorkTime.Ticks + shiftDto.OtherWorkTime.Ticks + shiftDto.DriveTime.Ticks) );
+	}
 }
