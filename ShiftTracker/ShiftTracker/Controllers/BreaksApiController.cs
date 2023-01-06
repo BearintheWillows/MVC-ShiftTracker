cf10461f72063ac1714c7fa0658f98f48d01f813
Stuart Willows <SJWillows91@outlook.com>
2023-01-06 18:57:06 +0000

    Added Shop Controller

--- ShiftTracker/ShiftTracker/Controllers/BreaksApiController.cs
+++ ShiftTracker/ShiftTracker/Controllers/BreaksApiController.cs
@@ -2,8 +2,143 @@
 
 namespace ShiftTracker.Controllers;
 
-[ApiController, Route("api/breaks")]
+using Areas.Shifts.Models.DTO;
+using Data;
+using Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+[ApiController, Route( "api/breaks" )]
 public class BreaksApiController : ControllerBase
 {
-	
+	private readonly ApplicationDbContext _context;
+
+	public BreaksApiController(ApplicationDbContext context)
+	{
+		_context = context;
+	}
+
+	[HttpGet]
+	public async Task<ActionResult<IEnumerable<Break>>> GetBreaks()
+	{
+		try
+		{
+			var breaks = await _context.Breaks.ToListAsync();
+
+			var breaksDto = breaks.Select( b => new BreakDto
+					{
+					Id = b.Id,
+					StartTime = b.StartTime,
+					EndTime = b.EndTime,
+					Duration = b.EndTime - b.StartTime,
+					ShiftId = b.ShiftId,
+					}
+			).ToList();
+
+			return Ok( breaksDto );
+		}
+		catch ( Exception e )
+		{
+			return BadRequest( e.Message );
+		}
+	}
+
+	[HttpGet( "{id}" )]
+	public async Task<ActionResult<Break>> GetBreak(int id)
+	{
+		try
+		{
+			var returnedBreak = await _context.Breaks.FindAsync( id );
+
+			if ( returnedBreak == null ) return NotFound( null );
+			var breakDto = new BreakDto
+				{
+				Id = returnedBreak.Id,
+				StartTime = returnedBreak.StartTime,
+				EndTime = returnedBreak.EndTime,
+				Duration = returnedBreak.EndTime - returnedBreak.StartTime,
+				ShiftId = returnedBreak.ShiftId,
+				};
+
+			return Ok( breakDto );
+		}
+		catch ( Exception e )
+		{
+			return BadRequest( e.Message );
+		}
+	}
+
+	[HttpPost]
+	public async Task<ActionResult<Break>> PostBreak(BreakDto breakDto)
+	{
+		try
+		{
+			if ( _context.Shifts.Any( s => s.Id == breakDto.ShiftId ) == false )
+				return BadRequest( "Shift does not exist" );
+			if ( _context.Breaks.Any( b => b.Id == breakDto.Id ) ) return BadRequest( "Break already exists" );
+
+			var newBreak = new Break
+				{
+				StartTime = breakDto.StartTime,
+				EndTime = breakDto.EndTime,
+				Duration = breakDto.EndTime - breakDto.StartTime,
+				ShiftId = breakDto.ShiftId,
+				};
+
+			_context.Breaks.Add( newBreak );
+			await _context.SaveChangesAsync();
+
+			return Ok( newBreak );
+		}
+		catch ( Exception e )
+		{
+			return BadRequest( e.Message );
+		}
+	}
+
+	[HttpDelete( "delete/{id}" )]
+	public async Task<ActionResult<Break>> DeleteBreak(int id)
+	{
+		try
+		{
+			var returnedBreak = _context.Breaks.Find( id );
+
+			if ( returnedBreak == null ) return NotFound( "Break does not exist" );
+
+			_context.Breaks.Remove( returnedBreak );
+			await _context.SaveChangesAsync();
+
+			return Ok( returnedBreak );
+		}
+		catch ( Exception e )
+		{
+			return BadRequest( e.Message );
+		}
+	}
+
+	[HttpPut( "update/{id}" )]
+	public async Task<ActionResult<Break>> PutBreak(int id, [FromBody] BreakDto breakDto)
+	{
+		try
+		{
+			if ( _context.Breaks.Any( b => b.Id == breakDto.Id ) == false ) return BadRequest( "Break does not exist" );
+
+			var returnedBreak = await _context.Breaks.FindAsync( id );
+
+			if ( _context.Shifts.Any( s => s.Id == returnedBreak!.ShiftId ) == false )
+				return BadRequest( "Shift does not exist" );
+
+			returnedBreak.StartTime = breakDto.StartTime;
+			returnedBreak.EndTime = breakDto.EndTime;
+			returnedBreak.Duration = breakDto.EndTime - breakDto.StartTime;
+
+			_context.Breaks.Update( returnedBreak );
+			await _context.SaveChangesAsync();
+
+			return Ok( returnedBreak );
+		}
+		catch ( Exception e )
+		{
+			return BadRequest( e.Message );
+		}
+	}
 }
---
+++ ShiftTracker/ShiftTracker/Controllers/ShopApiController.cs
@@ -0,0 +1,177 @@
+﻿namespace ShiftTracker.Controllers;
+
+using Areas.Shifts.Models.DTO;
+using Data;
+using Data.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+using Services;
+
+[ApiController, Route( "api/shops" )]
+public class ShopApiController : ControllerBase
+{
+	private readonly IShopService _shopService;
+
+	public ShopApiController(IShopService shopService)
+	{
+		_shopService = shopService;
+	}
+	[HttpGet]
+	public async Task<ActionResult<List<ShopDto>>> GetAllShifts([FromQuery] bool includeDayData = false)
+	{
+		var shops = new List<Shop>();
+		try
+		{
+			switch ( includeDayData )
+			{
+			case true:
+				shops = await _shopService.GetAllShopsWithDayData() as List<Shop>;
+				break;
+			default:
+				shops = await _shopService.GetAllAsync() as List<Shop>;
+				break;
+
+			}
+
+			if ( shops.Count == 0 )
+			{
+				return NotFound();
+			}
+
+			var shopResultDto = shops.Select( s => new ShopDto
+					{
+					Id = s.Id,
+					Street = s.Street,
+					Street2 = s.Street2,
+					City = s.City,
+					County = s.County,
+					Postcode = s.Postcode,
+					PhoneNumber = s.PhoneNumber,
+					DayVariants = includeDayData
+						? s.DayVariants.Select( dv => new DayVariantDto
+								{
+								Id = dv.Id,
+								ShopId = dv.ShopId,
+								DayOfWeek = dv.DayOfWeek,
+								WindowOpenTime = dv.WindowOpenTime,
+								WindowCloseTime = dv.WindowCloseTime,
+								RunId = dv.RunId
+								}
+						).ToList()
+						: null,
+					}
+			).ToList();
+
+			return Ok( shopResultDto );
+		}
+		catch ( Exception e )
+		{
+			Log.Error( e, "Error getting shifts" );
+			Console.WriteLine( e );
+			return BadRequest();
+		}
+	}
+}
+
+// [HttpGet("{id}")]
+	// public IActionResult GetShiftById(int id, [FromQuery] bool includeDayData = false)
+	// {
+	// 	try
+	// 	{
+	// 		Shop? shopResultAsync = _dbContext.Shops.AsQueryable()
+	// 		                                       .IncludeDayVariants( includeDayData )
+	// 		                                       .FirstOrDefault( s => s.Id == id );
+	//
+	// 		if ( shopResultAsync == null )
+	// 		{
+	// 			return new NotFoundResult();
+	// 		}
+	// 		
+	// 		var shopDto = new ShopDto
+	// 		{
+	// 			Id = shopResultAsync.Id,
+	// 			Street = shopResultAsync.Street,
+	// 			Street2 = shopResultAsync.Street2,
+	// 			City = shopResultAsync.City,
+	// 			County = shopResultAsync.County,
+	// 			Postcode = shopResultAsync.Postcode,
+	// 			PhoneNumber = shopResultAsync.PhoneNumber,
+	// 			DayVariants = includeDayData
+	// 				? shopResultAsync.DayVariants.Select(
+	// 					dv => new DayVariantDto
+	// 						{
+	// 						Id = dv.Id,
+	// 						ShopId = dv.ShopId,
+	// 						DayOfWeek = dv.DayOfWeek,
+	// 						WindowOpenTime = dv.WindowOpenTime,
+	// 						WindowCloseTime = dv.WindowCloseTime,
+	// 						RunId = dv.RunId
+	// 						}
+	// 				).ToList()
+	// 				: null,
+	// 		};
+	// 		
+	// 		return Ok( shopDto );
+	// 	}
+	// 	catch ( Exception e )
+	// 	{
+	// 		Log.Error( e, "Error getting shifts" );
+	// 		Console.WriteLine( e );
+	// 		return BadRequest();
+	// 	}
+	// }
+	//
+	// [HttpDelete("delete/{id}")]
+	// public async Task<IActionResult> DeleteShiftById(int id)
+	// {
+	// 	try
+	// 	{
+	// 		Shop? shopResultAsync = _dbContext.Shops.Find( id );
+	//
+	// 		if ( shopResultAsync == null )
+	// 		{
+	// 			return NotFound("No Shops found with that ID");
+	// 		}
+	// 		
+	// 		_dbContext.Shops.Remove( shopResultAsync );
+	// 		await _dbContext.SaveChangesAsync();
+	//
+	// 		return Ok( "Deleted Successfully" );
+	// 	}
+	// 	catch ( Exception e )
+	// 	{
+	// 		Log.Error( e, "Error getting shifts" );
+	// 		Console.WriteLine( e );
+	// 		return BadRequest("Error deleting shop");
+	// 	}
+	// }
+	//
+	// [HttpPost]
+	// public Task<IActionResult> CreateShop([FromBody] ShopDto shopDto)
+	// {
+	// 	try
+	// 	{
+	// 		Shop shop = new Shop
+	// 		{
+	// 			Street = shopDto.Street,
+	// 			Street2 = shopDto.Street2,
+	// 			City = shopDto.City,
+	// 			County = shopDto.County,
+	// 			Postcode = shopDto.Postcode,
+	// 			PhoneNumber = shopDto.PhoneNumber,
+	// 			DayVariants = new List<DayVariant>()
+	// 		};
+	// 		
+	// 		_dbContext.Shops.Add( shop );
+	// 		_dbContext.SaveChangesAsync();
+	//
+	// 		return Task.FromResult<IActionResult>( Ok( shop ) );
+	// 	}
+	// 	catch ( Exception e )
+	// 	{
+	// 		Log.Error( e, "Error getting shifts" );
+	// 		Console.WriteLine( e );
+	// 		return Task.FromResult<IActionResult>( BadRequest("Error creating shop") );
+	// 	}
+	//}
