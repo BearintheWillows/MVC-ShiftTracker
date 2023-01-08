a4bf1a6259965c99c4a28de8b8c9c0df1f5da5c5
Stuart Willows <SJWillows91@outlook.com>
2023-01-08 09:08:08 +0000

    Refactored all current methods to use ShiftService.

--- ShiftTracker/ShiftTracker/Controllers/ShiftsApiController.cs
+++ ShiftTracker/ShiftTracker/Controllers/ShiftsApiController.cs
@@ -90,102 +90,99 @@ public async Task<ActionResult<ShiftDto?>> GetShiftById(
 		return BadRequest( "Error getting shift" );
 	}
 }
+	
+
+		/// <summary>
+		/// Creates a new shift.
+		/// ShiftWithTimeDataDto is used to create a new shift with time data.
+		/// </summary>
+		/// <param name="shiftDto"></param>
+		/// <returns></returns>
+		[HttpPost]
+		public async Task<ActionResult?> AddShift([FromBody] ShiftDto shiftDto)
+		{
+			if ( !_shiftService.TimeEntryValidator( shiftDto ) )
+			{
+				return BadRequest( "Time entries do not add up to shift duration total" );
+			}
+
+			try
+			{
+				var shift = new Shift
+					{
+					Date = shiftDto.Date,
+					RunId = shiftDto.RunId,
+					Breaks = new List<Break>(),
+					StartTime = shiftDto.StartTime,
+					EndTime = shiftDto.EndTime,
+					BreakDuration = shiftDto.BreakDuration,
+					DriveTime = shiftDto.DriveTime,
+					ShiftDuration = shiftDto.ShiftDuration,
+					OtherWorkTime = shiftDto.OtherWorkTime,
+					WorkTime = shiftDto.WorkTime,
+					};
+				shift = await _shiftService.AddAsync( shift );
+				shiftDto.Id = shift.Id;
+				return Ok( shiftDto );
+			}
+			catch ( Exception e )
+			{
+				Log.Error( e, "Error creating shift" );
+				Console.WriteLine( e );
+				return BadRequest( null );
+			}
+		}
+
+		[HttpDelete( "{id}" )]
+		public async Task<ActionResult> DeleteShift(int id)
+		{
+			try
+			{
+				if ( !await _shiftService.ExistsAsync( id )) return NotFound( "Shift not found" );
+				
+				await _shiftService.DeleteAsync( id );
+				return Ok( "Shift Deleted Successfully" );
+			}
+			catch ( Exception e )
+			{
+				Log.Error( e, "Error deleting shift" );
+				Console.WriteLine( e );
+				return BadRequest( "Error deleting shift" );
+			}
+		}
+
+		[HttpPut( "{id}" )]
+	public async Task<IActionResult> UpdateShift(int id, [FromBody] ShiftDto shiftDto)
+	{
+		if ( !_shiftService.TimeEntryValidator( shiftDto ) )
+		{
+			return BadRequest("Time entries do not add up to shift duration total") ;
+		}
+		
+		try
+		{
+			var shift = await _shiftService.GetAsync( id );
+			if (shift == null)
+				return NotFound("Shift not found");
+			
+			shift.Date = shiftDto.Date;
+			shift.RunId = shiftDto.RunId;
+			shift.StartTime = shiftDto.StartTime;
+			shift.EndTime = shiftDto.EndTime;
+			shift.BreakDuration = shiftDto.BreakDuration;
+			shift.DriveTime = shiftDto.DriveTime;
+			shift.ShiftDuration = shiftDto.ShiftDuration;
+			shift.OtherWorkTime = shiftDto.OtherWorkTime;
+			shift.WorkTime = shiftDto.WorkTime;
+			
+			await _shiftService.UpdateAsync( shift );
+			return Ok("Shift Updated Successfully");
+		}
+		catch ( Exception e )
+		{
+			Log.Error( e, "Error updating shift" );
+			Console.WriteLine( e );
+			return BadRequest("Error updating shift");
+		}
+	}
 }
-// 	
-//
-// 	/// <summary>
-// 	/// Creates a new shift.
-// 	/// ShiftWithTimeDataDto is used to create a new shift with time data.
-// 	/// </summary>
-// 	/// <param name="shiftDto"></param>
-// 	/// <returns></returns>
-// 	[HttpPost]
-// 	public async Task<ActionResult?> AddShift([FromBody] ShiftDto shiftDto)
-// 	{
-// 		if ( !ShiftValidator.TimeEntryValidation( shiftDto ) )
-// 		{
-// 			return BadRequest("Time entries do not add up to shift duration total") ;
-// 		}
-// 		
-// 		try
-// 		{
-// 			var shift = new Shift
-// 					{
-// 					Date = shiftDto.Date,
-// 					RunId = shiftDto.RunId,
-// 					Breaks = new List<Break>(),
-// 					StartTime = shiftDto.StartTime,
-// 					EndTime = shiftDto.EndTime,
-// 					BreakDuration = shiftDto.BreakDuration,
-// 					DriveTime = shiftDto.DriveTime,
-// 					ShiftDuration = shiftDto.ShiftDuration,
-// 					OtherWorkTime = shiftDto.OtherWorkTime,
-// 					WorkTime = shiftDto.WorkTime,
-// 					};
-// 			await _dbContext.Shifts.AddAsync( shift );
-// 			await _dbContext.SaveChangesAsync();
-// 			shiftDto.Id = shift.Id;
-// 			return Ok(shiftDto);
-// 		}
-// 		catch ( Exception e )
-// 		{
-// 			Log.Error( e, "Error creating shift" );
-// 			Console.WriteLine( e );
-// 			return BadRequest(null);
-// 		}
-// 	}
-// 	
-// 	[HttpDelete("delete/{id}")]
-// 	public async Task<ActionResult> DeleteShift(int id)
-// 	{
-// 		try
-// 		{
-// 			var shift = await _dbContext.Shifts.FindAsync( id );
-// 			if (shift == null)
-// 				return NotFound("Shift not found");
-// 			_dbContext.Shifts.Remove( shift );
-// 			await _dbContext.SaveChangesAsync();
-// 			return Ok("Shift Deleted Successfully");
-// 		}
-// 		catch ( Exception e )
-// 		{
-// 			Log.Error( e, "Error deleting shift" );
-// 			Console.WriteLine( e );
-// 			return BadRequest("Error deleting shift");
-// 		}
-// 	}
-// 	
-// 	[HttpPut]
-// 	public async Task<IActionResult> UpdateShift([FromBody] ShiftDto shiftDto)
-// 	{
-// 		if ( !ShiftValidator.TimeEntryValidation( shiftDto ) )
-// 		{
-// 			return BadRequest("Time entries do not add up to shift duration total") ;
-// 		}
-// 		
-// 		try
-// 		{
-// 			var shift = await _dbContext.Shifts.FindAsync( shiftDto.Id );
-// 			if (shift == null)
-// 				return NotFound("Shift not found");
-// 			shift.Date = shiftDto.Date;
-// 			shift.RunId = shiftDto.RunId;
-// 			shift.StartTime = shiftDto.StartTime;
-// 			shift.EndTime = shiftDto.EndTime;
-// 			shift.BreakDuration = shiftDto.BreakDuration;
-// 			shift.DriveTime = shiftDto.DriveTime;
-// 			shift.ShiftDuration = shiftDto.ShiftDuration;
-// 			shift.OtherWorkTime = shiftDto.OtherWorkTime;
-// 			shift.WorkTime = shiftDto.WorkTime;
-// 			_dbContext.Shifts.Update( shift );
-// 			await _dbContext.SaveChangesAsync();
-// 			return Ok("Shift Updated Successfully");
-// 		}
-// 		catch ( Exception e )
-// 		{
-// 			Log.Error( e, "Error updating shift" );
-// 			Console.WriteLine( e );
-// 			return BadRequest("Error updating shift");
-// 		}
-// 	}
-// }
