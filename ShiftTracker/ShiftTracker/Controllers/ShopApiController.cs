c07ab6279e78b86feee9f992cb03f7f030688d3a
Stuart Willows <SJWillows91@outlook.com>
2023-01-08 08:09:58 +0000

    Added Update Shop Method.

--- ShiftTracker/ShiftTracker/Controllers/ShopApiController.cs
+++ ShiftTracker/ShiftTracker/Controllers/ShopApiController.cs
@@ -17,6 +17,7 @@ public class ShopApiController : ControllerBase
 	{
 		_shopService = shopService;
 	}
+
 	[HttpGet]
 	public async Task<ActionResult<List<ShopDto>>> GetAllShifts([FromQuery] bool includeDayData = false)
 	{
@@ -75,120 +76,161 @@ public class ShopApiController : ControllerBase
 		}
 	}
 
-	[HttpGet("{id}")]
-	 public async Task<IActionResult> GetShiftById(int id, [FromQuery] bool includeDayData = false)
-	 {
-	 	try
-	    {
-		    Shop? shopResultAsync;
+	[HttpGet( "{id}" )]
+	public async Task<IActionResult> GetShiftById(int id, [FromQuery] bool includeDayData = false)
+	{
+		try
+		{
+			Shop? shopResultAsync;
 
-		    if ( includeDayData == true )
-		    {
-			    shopResultAsync = await _shopService.GetShopWithDayData( id );
-		    }
-		    else
-		    {
-			    shopResultAsync = await _shopService.GetAsync( id );
-		    }
-		    
-	 		if ( shopResultAsync == null )
-	 		{
-	 			return new NotFoundResult();
-	 		}
-	 		
-	 		var shopDto = new ShopDto
-	 		{
-	 			Id = shopResultAsync.Id,
+			if ( includeDayData == true )
+			{
+				shopResultAsync = await _shopService.GetShopWithDayData( id );
+			} else
+			{
+				shopResultAsync = await _shopService.GetAsync( id );
+			}
+
+			if ( shopResultAsync == null )
+			{
+				return new NotFoundResult();
+			}
+
+			var shopDto = new ShopDto
+				{
+				Id = shopResultAsync.Id,
 				Name = shopResultAsync.Name,
 				Number = shopResultAsync.Number,
-	 			Street = shopResultAsync.Street,
-	 			Street2 = shopResultAsync.Street2,
-	 			City = shopResultAsync.City,
-	 			County = shopResultAsync.County,
-	 			Postcode = shopResultAsync.Postcode,
-	 			PhoneNumber = shopResultAsync.PhoneNumber,
-	 			DayVariants = includeDayData
-	 				? shopResultAsync.DayVariants.Select(
-	 					dv => new DayVariantDto
-	 						{
-	 						Id = dv.Id,
-	 						ShopId = dv.ShopId,
-	 						DayOfWeek = dv.DayOfWeek,
-	 						WindowOpenTime = dv.WindowOpenTime,
-	 						WindowCloseTime = dv.WindowCloseTime,
-	 						RunId = dv.RunId
-	 						}
-	 				).ToList()
-	 				: null,
-	 		};
-	 		
-	 		return Ok( shopDto );
-	 	}
-	 	catch ( Exception e )
-	 	{
-	 		Log.Error( e, "Error getting shifts" );
-	 		Console.WriteLine( e );
-	 		return BadRequest("Error getting shifts");
-	 	}
-	 }
-	
-	 [HttpDelete("{id}")]
-	 public async Task<IActionResult> DeleteShiftById(int id)
-	 {
-	 	try
-	    {
-		    if ( await _shopService.ExistsAsync( id ) == false )
-	 		{
-	 			return NotFound("No Shops found with that ID");
-	 		}
+				Street = shopResultAsync.Street,
+				Street2 = shopResultAsync.Street2,
+				City = shopResultAsync.City,
+				County = shopResultAsync.County,
+				Postcode = shopResultAsync.Postcode,
+				PhoneNumber = shopResultAsync.PhoneNumber,
+				DayVariants = includeDayData
+					? shopResultAsync.DayVariants.Select( dv => new DayVariantDto
+							{
+							Id = dv.Id,
+							ShopId = dv.ShopId,
+							DayOfWeek = dv.DayOfWeek,
+							WindowOpenTime = dv.WindowOpenTime,
+							WindowCloseTime = dv.WindowCloseTime,
+							RunId = dv.RunId
+							}
+					).ToList()
+					: null,
+				};
 
-		    await _shopService.DeleteAsync( id );
-		    
-		    return Ok( "Deleted Successfully" );
-	 	}
-	 	catch ( Exception e )
-	 	{
-	 		Log.Error( e, "Error getting shifts" );
-	 		Console.WriteLine( e );
-	 		return BadRequest("Error deleting shop");
-	 	}
-	 }
+			return Ok( shopDto );
+		}
+		catch ( Exception e )
+		{
+			Log.Error( e, "Error getting shifts" );
+			Console.WriteLine( e );
+			return BadRequest( "Error getting shifts" );
+		}
+	}
 
-	 [HttpPost]
-	 public async Task<IActionResult> CreateShop([FromBody] ShopDto shopDto)
-	 {
-		 
-		 try
-		 {
-			 if ( await _shopService.IsNameAndNumberUnique( shopDto ) )
-			 {
-				 return BadRequest("Shop Already Exists!");
-			 }
-			 
-			 Shop shop = new Shop
-				 
-				 {
-				 Id = shopDto.Id,
-				 Name = shopDto.Name,
-				 Number = shopDto.Number,
-				 Street = shopDto.Street,
-				 Street2 = shopDto.Street2,
-				 City = shopDto.City,
-				 County = shopDto.County,
-				 Postcode = shopDto.Postcode,
-				 PhoneNumber = shopDto.PhoneNumber,
-				 DayVariants = new List<DayVariant>()
-				 };
+	[HttpDelete( "{id}" )]
+	public async Task<IActionResult> DeleteShiftById(int id)
+	{
+		try
+		{
+			if ( await _shopService.ExistsAsync( id ) == false )
+			{
+				return NotFound( "No Shops found with that ID" );
+			}
 
-			 await _shopService.UpdateAsync(  shop );
+			await _shopService.DeleteAsync( id );
 
-			 return Ok(shop);
-		 }
-		 catch ( Exception e )
-		 {
-			 Log.Error( e, "Error getting shifts" );
-			 Console.WriteLine( e );
-			 return BadRequest("Error creating shop");
-		 }
-	 }
+			return Ok( "Deleted Successfully" );
+		}
+		catch ( Exception e )
+		{
+			Log.Error( e, "Error getting shifts" );
+			Console.WriteLine( e );
+			return BadRequest( "Error deleting shop" );
+		}
+	}
+
+	[HttpPost]
+	public async Task<IActionResult> CreateShop([FromBody] ShopDto shopDto)
+	{
+
+		try
+		{
+			if ( await _shopService.IsNameAndNumberUnique( shopDto ) )
+			{
+				return BadRequest( "Shop Already Exists!" );
+			}
+
+			Shop shop = new Shop
+				{
+				Id = shopDto.Id,
+				Name = shopDto.Name,
+				Number = shopDto.Number,
+				Street = shopDto.Street,
+				Street2 = shopDto.Street2,
+				City = shopDto.City,
+				County = shopDto.County,
+				Postcode = shopDto.Postcode,
+				PhoneNumber = shopDto.PhoneNumber,
+				DayVariants = new List<DayVariant>()
+				};
+
+			await _shopService.AddAsync( shop );
+
+			return Ok( shop );
+		}
+		catch ( Exception e )
+		{
+			Log.Error( e, "Error getting shifts" );
+			Console.WriteLine( e );
+			return BadRequest( "Error creating shop" );
+		}
+	}
+
+	[HttpPut]
+	public async Task<IActionResult> UpdateShop([FromBody] ShopDto shopDto)
+	{
+		
+		if ( await _shopService.ExistsAsync( shopDto.Id ) == false )
+		{
+			return NotFound( "No Shops found with that ID" );
+		}
+
+		if ( await _shopService.IsNameAndNumberUnique( shopDto ) )
+		{
+			return BadRequest( "Shop Already Exists! Name and Number must be unique" );
+		}
+		
+		Shop shop = new Shop
+			{
+			Id = shopDto.Id,
+			Name = shopDto.Name,
+			Number = shopDto.Number,
+			Street = shopDto.Street,
+			Street2 = shopDto.Street2,
+			City = shopDto.City,
+			County = shopDto.County,
+			Postcode = shopDto.Postcode,
+			PhoneNumber = shopDto.PhoneNumber,
+			};
+		
+		try
+		{
+
+			await _shopService.UpdateAsync( shop );
+
+			return Ok( shop );
+		}
+		catch ( Exception e )
+		{
+			Log.Error( e, "Error getting shifts" );
+			Console.WriteLine( e );
+			return BadRequest( "Error updating shop" );
+		}
+		
+		
+	}
 }
