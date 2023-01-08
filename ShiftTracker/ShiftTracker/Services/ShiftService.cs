8d8ad26d29207b91cb52cd8268f58e22c1c8ad4d
Stuart Willows <SJWillows91@outlook.com>
2023-01-08 09:08:34 +0000

    Moved TimeEntryValidator into ShiftService

--- ShiftTracker/ShiftTracker/Services/ShiftService.cs
+++ ShiftTracker/ShiftTracker/Services/ShiftService.cs
@@ -1,5 +1,6 @@
 ﻿namespace ShiftTracker.Services;
 
+using Areas.Shifts.Models.DTO;
 using Controllers;
 using Data;
 using Data.Models;
@@ -11,6 +12,8 @@ public interface IShiftService : IBaseCrudService<Shift>
 	Task<Shift> GetAsync(int id, bool includeBreaks,bool includeRun,bool includeTimeData);
 	Task<List<Shift>> GetAllAsync(bool includeBreaks, bool includeRun, bool includeTimeData);
 	Task<bool>        ExistsAsync(int  id);
+
+	bool TimeEntryValidator(ShiftDto shiftDto);
 }
 
 public class ShiftService : BaseCrudService<Shift>, IShiftService
@@ -46,5 +49,15 @@ public class ShiftService : BaseCrudService<Shift>, IShiftService
 	public async Task<bool> ExistsAsync(int id)
 	{
 		return await _context.Shifts.AnyAsync(x => x.Id == id);
+	}
+	
+	/// <summary>
+	/// Checks if the shift times add up to the shift duration.
+	/// </summary>
+	/// <param name="shiftDto"></param>
+	/// <returns>True/False</returns>
+	public bool TimeEntryValidator(ShiftDto shiftDto)
+	{
+		return shiftDto.ShiftDuration.Equals( new TimeSpan(shiftDto.BreakDuration.Ticks + shiftDto.WorkTime.Ticks + shiftDto.OtherWorkTime.Ticks + shiftDto.DriveTime.Ticks) );
 	}
 }
