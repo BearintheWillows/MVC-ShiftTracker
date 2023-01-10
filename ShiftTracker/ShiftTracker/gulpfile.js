d4f7ad98db0ef5c9eec56318f3196ebcac67db9c
Stuart Willows <SJWillows91@outlook.com>
2023-01-10 21:14:56 +0000

    Added Sass to Project

--- ShiftTracker/ShiftTracker/gulpfile.js
+++ ShiftTracker/ShiftTracker/gulpfile.js
@@ -5,6 +5,7 @@ Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
 */
 var gulp = require("gulp");
 var del = require("del");
+var sass = require("gulp-sass")(require("sass"));
 var paths = {
     scripts: ["scripts/**/*.js", "scripts/**/*"],
 };
@@ -14,4 +15,17 @@ gulp.task("clean", function () {
 gulp.task("default", function (done) {
     gulp.src(paths.scripts).pipe(gulp.dest("wwwroot/scripts"));
     done();
+});
+
+gulp.task('sass', function() {
+    return gulp.src("./Content/*.scss")
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./wwwroot/css'));
+});
+
+/*
+ * Watch prefixes
+ */
+gulp.task('watch', function() {
+    gulp.watch('.Content/*.scss', gulp.series('sass'));
 });
