27dbe62bda526b04979a3d968117497af342e4df
Stuart Willows <SJWillows91@outlook.com>
2023-01-11 19:19:48 +0000

    Added GetallbyShiftId for breaks

--- ShiftTracker/ShiftTracker/Services/BreakService.cs
+++ ShiftTracker/ShiftTracker/Services/BreakService.cs
@@ -7,7 +7,8 @@ using Microsoft.EntityFrameworkCore;
 
 public interface IBreakService : IBaseCrudService<Break>
 {
-	Task<bool> ExistsAsync(int? id);
+	Task<bool>               ExistsAsync(int?         id);
+	Task<IEnumerable<Break>> GetAllAsyncByShiftId(int shiftId);
 }
 
 public class BreakService : BaseCrudService<Break>, IBreakService
@@ -22,5 +23,10 @@ public class BreakService : BaseCrudService<Break>, IBreakService
 	public async Task<bool> ExistsAsync(int? id)
 	{
 		return await _context.Breaks.AnyAsync( s => s.Id == id);
+	}
+	
+	public async Task<IEnumerable<Break>> GetAllAsyncByShiftId(int shiftId)
+	{
+		return await _context.Breaks.Where( s => s.ShiftId == shiftId).ToListAsync();
 	}
 }
