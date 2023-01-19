0333bc5cf9b36c766b594c7053d7852f662d60db
Stuart Willows <SJWillows91@outlook.com>
2023-01-19 13:24:18 +0000

    Added Remove Break button

--- ShiftTracker/ShiftTracker/src/typescript/components/breakForm.ts
+++ ShiftTracker/ShiftTracker/src/typescript/components/breakForm.ts
@@ -5,105 +5,96 @@
     constructor() {
     }
 
-    displayRow() {
+
+    displayAllRows() {
         const form = document.getElementById("breakForm");
         form.innerHTML = "";
-        if(this.breaks.length < 1) {
-            form.append(this.createRow(undefined, undefined, "add"));
-        } else {
-        }
+        form.append(this.createAddBreakButton())
+        console.log(this.breaks);
+
+        this.breaks.forEach((b) => {
+            form.appendChild(this.createRow(b, this.breaks.indexOf(b)));
+        });
+    }
+
+    createTimeInput(i: number, timeType: string): HTMLInputElement {
+        const input = document.createElement("input");
+        input.type = "time";
+        input.id = `${timeType}-${i}`;
+        input.addEventListener('change', () => {
+            this.updateBreak(input.value, i, timeType);
+        })
+        return input;
     }
     
-    displayAllRows(){
-        const form = document.getElementById("breakForm");
-        form.innerHTML = "";
-        if(this.breaks.length < 1) {
-            form.append(this.createRow(undefined, undefined, "add"));
-        } else {
-            for(let i = 0; i < this.breaks.length; i++) {
-                if(i === this.breaks.length - 1 && this.breaks.length > 1) {
-                    form.append(this.createRow(this.breaks[i].startTime, this.breaks[i].endTime, "addAndRemove"));
-                } else {
-                    form.append(this.createRow(this.breaks[i].startTime, this.breaks[i].endTime, "remove"));
-                }
-            }
-        }
-        
-    }
-
-    createTimeInput(startOrEnd: string, time?: Date) {
-        const input = document.createElement("input") as HTMLInputElement;
-        const label = document.createElement("label") as HTMLLabelElement;
-        const timeInputDiv = document.createElement("div") as HTMLDivElement;
-
-        label.textContent = startOrEnd ? "Start Time: " : "End Time: ";
-        label.setAttribute("for", startOrEnd ? "startTime" : "endTime");
-        input.type = "Time";
-        if(startOrEnd === "startTime") {
-            input.id = "startTime";
-        } else if(startOrEnd === "endTime") {
-            input.id = "endTime";
-        }
-        input.id = (this.breaks.length - 1).toString();
-
-        timeInputDiv.append(label, input);
-
-        return timeInputDiv
-    }
-
-    createRemoveBreakButton() {
-        const button = document.createElement("button") as HTMLButtonElement;
-        button.textContent = "Remove";
-        button.addEventListener("click", (event) => {
-            event.preventDefault();
-            let element = event.target as HTMLElement;
-            let id = element.parentElement.id;
-            this.breaks.splice(parseInt(id), 1);
-            console.log (this.breaks);
-
-        });
-        return button;
-    }
-
-    createAddBreakButton() {
-        const button = document.createElement("button") as HTMLButtonElement;
-        button.textContent = "Add";
-        button.addEventListener("click", (event) => {
-            event.preventDefault();
-            
-            this.addBreak(new Date(), new Date());
+    private createAddBreakButton(): HTMLButtonElement {
+        const button = document.createElement("button");
+        button.innerText = "Add Break";
+        button.classList.add("addBreakButton");
+        button.type = "button";
+        button.addEventListener('click', () => {
+            this.breaks.push({startTime: null, endTime: null, duration: null});
             this.displayAllRows();
         });
+        
         return button;
     }
 
-    createRow(startTime?: Date, endTime?: Date, buttonType?: string) {
-        const row = document.createElement("div") as HTMLDivElement;
-
-        if(buttonType === "addAndRemove") {
-            row.append(this.createTimeInput("startTime", startTime ? startTime : undefined), this.createTimeInput("endTime", endTime ? endTime : undefined), this.createRemoveBreakButton());
-        } else if(buttonType === "remove") {
-            row.append(this.createTimeInput("startTime", startTime ? startTime : undefined), this.createTimeInput("endTime", endTime ? endTime : undefined), this.createRemoveBreakButton(), this.createAddBreakButton());
-        } else if(buttonType === "add") {
-            row.append(this.createTimeInput("startTime", startTime ? startTime : undefined), this.createTimeInput("endTime", endTime ? endTime : undefined), this.createAddBreakButton());
-        }
-        row.classList.add("breakForm__row");
+    private createRow(b?: IBreak, i?: number): HTMLDivElement {
+        const row = document.createElement("div");
+        row.className = "breakRow";
+        row.id = `breakRow-${i}`;
+        let startTime = this.createTimeInput(i, "startTime");
+        let endTime = this.createTimeInput(i, "endTime");
+        let removeButton = this.CreateRemoveButton(i);
+        row.append(startTime, endTime, removeButton);
         return row;
     }
+    
+    private CreateRemoveButton(i: number): HTMLButtonElement {
+        const button = document.createElement("button");
+        button.innerText = "Remove";
+        button.classList.add("removeBreakButton");
+        button.type = "button";
+        button.addEventListener('click', () => {
+            this.breaks.splice(i, 1);
+            this.displayAllRows();
+        });
 
-    addBreak(startTime: Date, endTime: Date) {
-        const newBreak = {
-            startTime: startTime,
-            endTime: endTime,
-            duration: this.calculateDuration(startTime, endTime)
+        return button;
+    }
+        
+
+
+    private formatTime(value: string) {
+        return new Date(value);
+    }
+
+    private updateBreak(date: string, i: number, timeType: string) {
+        
+        if (timeType === "startTime") {
+            this.breaks[i].startTime = this.dateFormat(date);
+        } else {
+            this.breaks[i].endTime = this.dateFormat(date);
         }
-        console.log(newBreak);
-        console.log (this.breaks);
-        this.breaks.push(newBreak);
+        
+        if(this.breaks[i].startTime != null && this.breaks[i].endTime != null) {
+            this.breaks[i].duration = this.calculateDuration(this.breaks[i].startTime, this.breaks[i].endTime);
+            document.querySelector(`#breakRow-${i}`).removeChild(document.querySelector("button"));
+            this.displayAllRows();
+        }
+        console.log(this.breaks);
     }
 
     private calculateDuration(startTime: Date, endTime: Date) {
-        const duration: Date = new Date( endTime.getTime() - startTime.getTime());
-        return duration;
+        let duration = (endTime.getTime() - startTime.getTime()) / (1000 * 60);
+        let hours = Math.floor(duration / 60);
+        let minutes = duration % 60;
+        return new Date(0, 0, 0, hours, minutes, 0, 0);
+    }
+
+    private dateFormat(date: string) {
+        let timeArray = date.split(":");
+        return new Date(0, 0, 0, parseInt(timeArray[0]), parseInt(timeArray[1]), 0, 0);
     }
 }
