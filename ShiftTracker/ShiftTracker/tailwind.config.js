d5c65aacfe17db11a86cbdf3c94a47df8fdf2fdf
Stuart Willows <SJWillows91@outlook.com>
2023-01-23 14:23:37 +0000

    Refactored... webpack/postcss

--- ShiftTracker/ShiftTracker/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./**/*.cshtml', './**/*.ts'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
---
+++ ShiftTracker/ShiftTracker/webpack.config.js
@@ -0,0 +1,31 @@
+﻿const path = require('path');
+
+module.exports = {
+    entry: './src/typescript/main.ts',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1,
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader'
+                    }
+                ]
+            }
+        ]
+    }
+    output: {
+        filename: [__filename, 'bundle.js'].join(''),
+        path: path.resolve(__dirname, './wwwroot/dist'),
+    },
+};
